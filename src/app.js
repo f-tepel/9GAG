0cd0d76888a7c9188fcd7b2fe234de59d5df3eb3
fTepel <felix.tepel@gmail.com>
2018-11-03 18:31:17 +0100

    add register

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = process.env.PORT || 8080
 
 const pages = require('./pages')
+const setters = require('./setters')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use(pages)
+app.use(setters)
+
+MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+    if(err) {
+        return console.log('connection failed')
+    }
+    const db = client.db('9GAG');
+    (global).User = db.collection('User');
+})
 
 console.log('Up and running')
 
--- src/setters.js
+++ src/setters.js
@@ -2,12 +2,20 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const router = express.Router()
 
+router.use(bodyParser.urlencoded({ extended: true }))
 router.use(bodyParser.json())
-router.use(bodyParser.urlencoded({ extended: false }))
 
 router.post('/user', (req, res) => {
     let name = req.body.name
     let email = req.body.email
     let pwd = req.body.pwd
-    console.log(name + ' | ' + email + ' | ' + pwd)
+    console.log(name + ' | ' + email + ' | ' + pwd);
+    (global).User.insert({
+        name,
+        email,
+        pwd
+    })
+    res.status(200).send('Added User')
 })
+
+module.exports = router
